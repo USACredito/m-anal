7bac065ccda02be7bb3cd59165867cac03112e4b
adverso88 <adversoagencia@gmail.com>
2026-03-30 21:38:37 -0500

    Disable debug mode from app.run to avoid Werkzeug reload crashes in docker server

--- dashboard/app.py
+++ dashboard/app.py
@@ -321,4 +321,4 @@ if __name__ == "__main__":
 ║  URL:    http://localhost:{PORT:<27}║
 ╚══════════════════════════════════════════════════════╝
 """)
-    app.run(host="0.0.0.0", port=PORT, debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=False)
